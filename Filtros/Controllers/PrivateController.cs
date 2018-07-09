5ee5a46c551b0b10d0d6bf7d17b95ca9fa7cd6c8
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-07-09 23:56:18 +0200

    Añado el controlador private

---
+++ Filtros/Controllers/PrivateController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filtros.Controllers
+{
+    [Route("private")]
+    [Authorize]
+    public class PrivateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Private index");
+        }
+    }
+}
