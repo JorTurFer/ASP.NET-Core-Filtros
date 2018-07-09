ae540ad44de75fc7c90340d30b7a81cef0425aca
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-07-09 23:29:15 +0200

    añado boton para login y codigo post

--- Filtros/Controllers/AccountController.cs
+++ Filtros/Controllers/AccountController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Filtros.Viewmodels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Filtros.Controllers
@@ -13,11 +17,30 @@ namespace Filtros.Controllers
         {
             return View();
         }
-        [Route("login")]
+
         [HttpGet]
+        [Route("login")]
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(LoginViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+                identity.AddClaim(new Claim(ClaimTypes.Name, vm.Username));
+                var principal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme, principal
+                );
+                return RedirectToAction("Index", "Home");
+            }
+            vm.ErrorMessage = "Not authorized";
+            return View(vm);
         }
     }
 }
